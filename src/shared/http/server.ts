c0598bee9294d1e318be9cde57bfcf00e9fec776
kauemurakami <kauetmurakami@gmail.com>
2021-02-16 16:17:09 -0300

    primeira exucução e upload do readme

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,1 +1,13 @@
-console.log('Hello Dev!');
+import express from 'express';
+import cors from 'cors';
+import routes from './routes';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log('Server Started on port 3333 👯‍♂️');
+});
