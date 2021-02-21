8b48b7705d697d4bd34d3521c977da7cf2a5bd25
kauemurakami <kauetmurakami@gmail.com>
2021-02-21 14:52:17 -0300

    criando serviço de criar produto

--- src/modules/products/typeorm/repositories/ProductsRepository.ts
+++ src/modules/products/typeorm/repositories/ProductsRepository.ts
@@ -3,10 +3,8 @@ import Product from '../entities/Product';
 
 @EntityRepository(Product)
 export class ProductRepository extends Repository<Product> {
-
   public async findByName(name: string): Promise<Product | undefined> {
     const product = this.findOne({ where: { name } });
     return product;
   }
-
 }
