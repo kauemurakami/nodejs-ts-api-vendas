b28b96a493eb1215ba7fc15ce0ae10c2b572c7d2
kauemurakami <kauetmurakami@gmail.com>
2021-02-27 19:47:26 -0300

    criando serviço de upload de imagem

---
+++ src/modules/users/services/UpdateUserAvatarService.ts
@@ -0,0 +1,33 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import path from 'path';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+import uploadConfig from '@config/upload';
+import fs from 'fs';
+
+interface IRequest {
+  user_id: string;
+  avatarFilename: string;
+}
+
+class UpdateUserService {
+  public async execute({ user_id, avatarFilename }: IRequest): Promise<User> {
+    const userRepository = getCustomRepository(UsersRepository);
+    const user = await userRepository.findById(user_id);
+    if (!user) {
+      throw new AppError('Usuário não encontrado');
+    }
+    if (user.avatar) {
+      const userAvatarFilePath = path.join(uploadConfig.directory, user.avatar);
+      const userAvatarFileExists = await fs.promises.stat(userAvatarFilePath);
+      if (userAvatarFileExists) {
+        await fs.promises.unlink(userAvatarFilePath);
+      }
+    }
+    user.avatar = avatarFilename;
+    await userRepository.save(user);
+    return user;
+  }
+}
+export default UpdateUserService;
