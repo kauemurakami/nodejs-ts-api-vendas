6d89e309bed9b7ab2c1220e0e1774265944eea58
kauemurakami <kauetmurakami@gmail.com>
2021-02-27 19:34:53 -0300

    adicionando arquivvo de configuração de upload de imagens

--- src/shared/http/middlewares/isAuthenticated.ts
+++ src/shared/http/middlewares/isAuthenticated.ts
@@ -3,7 +3,7 @@ import { verify } from 'jsonwebtoken';
 import AppError from '@shared/errors/AppError';
 import authConfig from '@config/auth';
 
-interface TokenPayload {
+interface ITokenPayload {
   iat: number;
   exp: number;
   sub: string;
@@ -23,7 +23,7 @@ export default function isAuthenticated(request: Request, response: Response, ne
     //verifica se esse token foi criado com essa secret
     const decodedToken = verify(token, authConfig.jwt.secret);
     // console.log(decodedToken);
-    const { sub } = decodedToken as TokenPayload;
+    const { sub } = decodedToken as ITokenPayload;
     request.user = {
       id: sub,
     };
