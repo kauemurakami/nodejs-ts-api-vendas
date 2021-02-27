7eebee80bdab2aa4fe91966bc5f770165ca301d1
kauemurakami <kauetmurakami@gmail.com>
2021-02-27 17:02:15 -0300

    adicionando services, service de criação de usaurio

---
+++ src/modules/users/services/CreateUserService.ts
@@ -0,0 +1,25 @@
+import AppError from '@shared/errors/AppError';
+import { getCustomRepository } from 'typeorm';
+import User from '../typeorm/entities/User';
+import UsersRepository from '../typeorm/repositories/UsersRepository';
+
+interface IRequest {
+  name: string;
+  email: string;
+  password: string;
+}
+
+class CreateUserService {
+  public async execute({ name, email, password }: IRequest): Promise<User> {
+    const userRepository = getCustomRepository(UsersRepository);
+    const emailExists = await userRepository.findByEmail(email);
+    if(emailExists){
+      throw new AppError('Email já existente.');
+    }
+    const user = userRepository.create({ name, email, password });
+
+    await userRepository.save(user);
+    return user;
+  }
+}
+export default CreateUserService;
