8f82f56d0be24c8b9758e8c8e63c801b09a34bd0
kauemurakami <kauetmurakami@gmail.com>
2021-02-28 15:25:53 -0300

    enviando email para ethereal

---
+++ src/config/mail/ethereal_mail.ts
@@ -0,0 +1,31 @@
+import nodemailer from 'nodemailer';
+
+interface ISendMail {
+  to: string;
+  body: string;
+}
+
+export default class EtherealMail {
+  static async sendMail({to, body}: ISendMail): Promise<void> {
+    const account = await nodemailer.createTestAccount();
+
+    const transporter = nodemailer.createTransport({
+      host: account.smtp.host,
+      port: account.smtp.port,
+      secure: account.smtp.secure,
+      auth: {
+        user: account.user,
+        pass: account.pass,
+      },
+    });
+    const message = await transporter.sendMail({
+      from: 'deebx@gmail.com',
+      to,
+      subject: 'Recuperação de senha',
+      text: body,
+    });
+
+    console.log('Message sent: %s', message.messageId);
+    console.log('Preview URL: %s', nodemailer.getTestMessageUrl(message));
+  }
+}
--- src/modules/users/services/SendForgotPasswordEmailService.ts
+++ src/modules/users/services/SendForgotPasswordEmailService.ts
@@ -2,6 +2,7 @@ import AppError from '@shared/errors/AppError';
 import { getCustomRepository } from 'typeorm';
 import UserTokensRepository from '../typeorm/repositories/UserTokenRepository';
 import UsersRepository from '../typeorm/repositories/UsersRepository';
+import EtherealMail from '@config/mail/ethereal_mail';
 
 interface IRequest {
   email: string;
@@ -15,9 +16,13 @@ class SendForgotPasswordEmailService {
     if (!user) {
       throw new AppError('Usuário não encontrado');
     }
-    console.log(user);
+    // console.log(user);
     const token = await userTokenRepository.generate(user.id);
     console.log(token);
+    await EtherealMail.sendMail({
+      to: email,
+      body: `Solicitação de redefinição de senha recebida: ${token?.token}`,
+    });
   }
 }
 export default SendForgotPasswordEmailService;
