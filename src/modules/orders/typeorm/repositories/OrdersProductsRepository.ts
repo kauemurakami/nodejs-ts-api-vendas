493b2131b4e8896db4be1cc89caa5d977219e847
kauemurakami <kauetmurakami@gmail.com>
2021-03-12 20:39:55 -0300

    criando repositorio de ordersproducts

---
+++ src/modules/orders/typeorm/repositories/OrdersProductsRepository.ts
@@ -0,0 +1,10 @@
+import { EntityRepository, Repository } from 'typeorm';
+import Product from '../entities/Product';
+
+@EntityRepository(Product)
+export class ProductRepository extends Repository<Product> {
+  public async findByName(name: string): Promise<Product | undefined> {
+    const product = this.findOne({ where: { name } });
+    return product;
+  }
+}
